34071d8af11d2fcb5c5b25d0bf1f2d59c180bd1b
DeepakTayde <deepaktayde559@gmail.com>
2024-03-21 12:08:07 +0530

    update in sidebar

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // Style
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import './index.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
+
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 
 
